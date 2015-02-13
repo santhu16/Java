2014d9fd4ddad460aa00358c8dd96bbf34a4fb3e
Gautham Honnavara Srinivasan <gauthamhs@gmail.com>
2015-02-13 15:24:56 -0500

    Create Thor.java

---
+++ Core-Java/Abstract-Classes/Thor.java
@@ -0,0 +1,19 @@
+//Thor child class extending Odin Parent class.
+
+public class Thor extends Odin {
+
+	@Override
+	public void strength() {
+		System.out.println("Thor's hammer");
+	}
+
+	@Override
+	public void attire() {
+		System.out.println("Red");
+	}
+
+	@Override
+	public void trait() {
+		System.out.println("Arrogant and Powerful");
+	}
+}
