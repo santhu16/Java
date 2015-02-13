52d9d7f8f14741ce65ff6fd0090fa1626c773024
Gautham Honnavara Srinivasan <gauthamhs@gmail.com>
2015-02-13 15:22:43 -0500

    Create Loki.java

---
+++ Core-Java/Abstract-Classes/Loki.java
@@ -0,0 +1,20 @@
+//Child Class Loki extending Parent class Odin.
+
+public class Loki extends Odin {
+
+	@Override
+	public void strength() {
+		System.out.println(" Loki's wit");
+	}
+
+	@Override
+	public void attire() {	
+		System.out.println("Dark Green");
+	}
+
+	@Override
+	public void trait() {
+		System.out.println("Cunning and Sarcastic");
+	}
+
+}
