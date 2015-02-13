fb31fdbadb85ef41ea2e7a512793228a67a9a54e
Gautham Honnavara Srinivasan <gauthamhs@gmail.com>
2015-02-13 15:23:46 -0500

    Create Odin.java

---
+++ Core-Java/Abstract-Classes/Odin.java
@@ -0,0 +1,23 @@
+//Abstract class Odin whose methods should be implemented by the respective child classses.
+public abstract class Odin {
+	private int id;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public abstract void strength();
+	public abstract void attire();
+	public abstract void trait();
+
+	public void run() {
+		strength();
+		attire();
+		trait();
+	}
+
+}
