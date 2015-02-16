f0797ae670ee77a80e15d17ea83e298c02ec2fc8
Gautham Honnavara Srinivasan <gauthamhs@gmail.com>
2015-02-16 17:18:05 -0500

    Create Application.java

---
+++ Core-Java/Classes-and-Objects/Application.java
@@ -0,0 +1,30 @@
+//Classes and Objects.
+
+class Person {
+	String name; // Data or Instance Variables.
+	int age;
+
+	public void speak() { // Class method: May be invoked in the main program
+		System.out.println("My name is " + name + " and my Age is " + age + "!!");
+	}
+}
+
+public class Application {
+	public static void main(String[] args) {
+
+		Person Person_1 = new Person(); // Syntax for creating a new class object.
+		Person_1.name = "Gautham Honnavara Srinivasan"; // Pointing the name of Person class to a string
+		Person_1.age = 23; // Pointing the age of Person class to an integer value
+		Person_1.speak();// Invoking class method
+
+		Person Person_2 = new Person(); // Multiple Class objects can be created for a single class.
+		Person_2.name = "Joe Hage"; 
+		Person_2.age = 46;
+		Person_2.speak();
+
+		/*
+		 * System.out.println(Person_1.name); System.out.println(Person_1.age);
+		 * System.out.println(Person_2.name); System.out.println(Person_2.age);
+		 */
+	}
+}
