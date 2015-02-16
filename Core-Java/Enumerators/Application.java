ca120e9e1bd97f29496ffb19dca1a3dbfdc32cb9
Gautham Honnavara Srinivasan <gauthamhs@gmail.com>
2015-02-16 18:40:10 -0500

    Update Application.java

--- Core-Java/Enumerators/Application.java
+++ Core-Java/Enumerators/Application.java
@@ -59,10 +59,13 @@ public class Application {
 		}
 
 		System.out.println(Colours.Blue); // Shows the enum variable when tostring() is not called.
+		                                  // o/p: tostring(), if not Blue
 		System.out.println(Colours.Blue.getClass()); // Displays the class that the color blue belongs to.
-		System.out.println(Colours.Blue.getName()); // Displays the name assigned to enum variable.
-		System.out.println(Colours.Blue.name()); // Displays the name of enum variable from java.lang.
-		
+		                                             // o/p: Colours
+		System.out.println(Colours.Blue.getName());  // Displays the name assigned to enum variable.
+		                                             // Gives the value of Enumerators.
+		System.out.println(Colours.Blue.name());     // Displays the name of enum variable from java.lang.
+		                                             // o/p Blue
 		System.out.println(Colours.Blue); // Displays toString() instead of enum variable.
 		
 		System.out.println(Colours.Blue instanceof Colours); // Compares whether the Blue object belongs to  
