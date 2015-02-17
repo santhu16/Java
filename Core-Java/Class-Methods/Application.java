83a9aaa63c925501b6cda6bff8cb389436b4e106
Gautham Honnavara Srinivasan <gauthamhs@gmail.com>
2015-02-17 10:36:28 -0500

    Update Application.java

--- Core-Java/Class-Methods/Application.java
+++ Core-Java/Class-Methods/Application.java
@@ -28,14 +28,14 @@ public class Application {
  
         // Create a Person object using the Person class
         Person person1 = new Person();
-        person1.name = "Joe Bloggs";
+        person1.name = "Gautham Honnavara";
         person1.age = 37;
         person1.speak();
         person1.sayHello();
         
         // Create a second Person object
         Person person2 = new Person();
-        person2.name = "Sarah Smith";
+        person2.name = "Sam NotWorthington";
         person2.age = 20;
         person2.speak();
         person1.sayHello();
