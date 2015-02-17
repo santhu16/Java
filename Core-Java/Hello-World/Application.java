dd244a4e71d325ff59a5770036e371f4784883b4
Gautham Honnavara Srinivasan <gauthamhs@gmail.com>
2015-02-17 11:37:14 -0500

    Create Application.java

---
+++ Core-Java/Hello-World/Application.java
@@ -0,0 +1,8 @@
+//My first Java Code- A simple hello world program !!
+
+public class Application {
+	public static void main(String[] args) {
+		System.out.println("Hello World !!"); // sysout is used to print a new statement and
+		                                      // println prints it in a new line.
+	}
+}
