85799fbdbd3242c51bec87aa3ddc9e1e3dec3bcc
Gautham Honnavara Srinivasan <gauthamhs@gmail.com>
2015-02-16 23:10:25 -0500

    Create Application.java

---
+++ Core-Java/For-Loop/Application.java
@@ -0,0 +1,11 @@
+//A simple For Loop. 
+// Unlike a while loop, in a for loop, the initialization, expression and incrementing the values
+// are all done inside a single bracket.
+
+public class Application {
+	public static void main(String[] args) {
+		for(int i=0;i<10;i++){ // Initialization, Expression and Incrementing.
+			System.out.println("Count: " + i);
+		}
+	}
+}
