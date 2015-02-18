05ce2afc56eb0626fb74ab8ac48685d15245290c
Gautham Honnavara Srinivasan <gauthamhs@gmail.com>
2015-02-18 11:34:45 -0500

    Create Application.java

---
+++ Core-Java/Serialization-and-Deserialization-Introduction/Application.java
@@ -0,0 +1,20 @@
+//Serialization is a process of translating objects into a binary format so that it can be stored for future purposes.
+
+import java.io.Serializable;
+
+public class Application implements Serializable { // Application class implements Serializable
+                                                   // interface to serialize objects.
+	
+	private static final long serialVersionUID = 1704785874013355194L;
+	private int id; // Variables id and name
+	private String name;
+
+	public Application(int id, String name) { // Constructor
+		this.id = id;
+		this.name = name;
+	}
+	@Override
+	public String toString() { // Displaying the variable.
+		return "Application [id = " + id + ", name = " + name + "]";
+	}
+}
