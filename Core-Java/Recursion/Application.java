f0e3cedbc8383f93ddac8e06d7e858f64ff1e8c6
Gautham Honnavara Srinivasan <gauthamhs@gmail.com>
2015-02-18 10:38:20 -0500

    Create Application.java

---
+++ Core-Java/Recursion/Application.java
@@ -0,0 +1,19 @@
+// Calculating the Factorial of a value using Recursion.
+
+public class Application {
+
+	public static void main(String[] args) {
+
+		System.out.println("The Factorial of the value(by recursion) is : " + factorial(6)); 
+		// Passes value 4 into the factorial method
+		// Expected output : 4*3*2*1 = 24;
+	}
+
+	public static int factorial(int value) {
+		//System.out.println(value);
+		if (value == 1) { // Check if Value = 1; If so, return 1;
+			return 1;
+		}
+		return factorial(value - 1) * value;
+	}
+}
